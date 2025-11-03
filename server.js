ec40fff98cf61af927427a36700af00566aa9530
Some random dude <125770478+crshqd@users.noreply.github.com>
2025-11-03 07:13:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
-const serverlol = "https://glcdn.githack.com/uiiuvdzfghvfd/a/-/raw/main/b";
+const serverlol = "https://gl.githack.com/uiiuvdzfghvfd/a/-/raw/main/b";
 // yo whats up whoevers seeing this
