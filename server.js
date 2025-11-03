f76e0ca86304c029a7f3c704a9531e63bf3f0c7f
Some random dude <125770478+crshqd@users.noreply.github.com>
2025-11-03 16:58:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-const serverlol = "https://gl.githack.com/uiiuvdzfghvfd/a/-/raw/main";
+const serverlol = "https://api.npoint.io/d051e27af08402781896?";
 const serverlol2 = "https://gl.githack.com/uiiuvdzfghvfd/a/-/raw/main/b";
 // yo whats up whoevers seeing this
