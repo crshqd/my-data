590c8afb2794c90a3901ce46b35f149fb7da884a
Some random dude <125770478+crshqd@users.noreply.github.com>
2025-11-03 16:33:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,3 @@
 const serverlol = "https://gl.githack.com/uiiuvdzfghvfd/a/-/raw/main/b";
+const serverlol2 = "https://gl.githack.com/uiiuvdzfghvfd/a/-/raw/main/b";
 // yo whats up whoevers seeing this
