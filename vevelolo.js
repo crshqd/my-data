9b584ce080aa4aa899c6d43e35e86dca3e7749eb
Some random dude <125770478+crshqd@users.noreply.github.com>
2026-01-12 15:11:32 -0500

    Update vevelolo.js

--- vevelolo.js
+++ vevelolo.js
@@ -1,1 +1,2 @@
 let vevelolo = "http://aetrymarkabiscudrijeauh.mywebcommunity.org/RespectKennethFromArt.php";
+alert("PSA my servers imploded because yall played too much\nNot your fault btw, trust me will be fixed")
